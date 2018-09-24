24e1bd5554ff9b2a8c35ec771a0a14d9e98b4d4e
bilnkat <billyjohn.general@gmail.com>
2018-09-24 08:14:52 -0700

    1

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import os
+from subprocess import Popen, PIPE
+
+
 
 home = os.path.expanduser('~')
 print(home)
-# os.system('ipconfig')
-
+os = subprocess.call('diskpart')
+print(os)
+# osDrive = type(subprocess.run('%homedrive%'))
+# # os.system('ipconfig')
+# print(osDrive)
